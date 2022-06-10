8347983b272c8b40a1987051c1b6c73cea3a0562
Kevin Hernandez <kevinh9999@gmail.com>
2022-06-09 20:56:48 -0400

    DH-20 completed ex.2

--- Kevin/py_class/lesson2/ex2-4.py
+++ Kevin/py_class/lesson2/ex2-4.py
@@ -1,3 +1,4 @@
+#creating vlans forom vlan file.
 from netmiko import ConnectHandler
 
 Nexus1 = {
@@ -12,15 +13,8 @@ Nexus2 = {
     "username": "kevin",
     "password": "kevin"
 }
-command1 =["ip name-server 8.8.8.8 use-vrf management",
-        "ip name-server 8.8.4.4 use-vrf management",
-        "ip domain-lookup",
-]
-ping ="ping 8.8.8.8 vrf management"
-
 for n in (Nexus1, Nexus2):
     ssh = ConnectHandler(**n)
-    print(ssh.send_command(ping))
-    print(ssh.send_config_set(command1))
-    print(ssh.find_prompt())
+    print(ssh.send_config_from_file("vlan.txt"))
+    print(ssh.save_config())
     
